cbadd5cb9e8a76cc87a41a23de5efcfcdb645dea
ashishhulke <71589148+ashishhulke@users.noreply.github.com>
2024-05-03 13:14:53 +0530

    Changes in orderprocessing

--- Solutions/ECommerce/OrderProcessing/Repositories/IOrderRepository.cs
+++ Solutions/ECommerce/OrderProcessing/Repositories/IOrderRepository.cs
@@ -4,7 +4,7 @@ namespace Transflower.ECommerce.OrderProcessing.Repositories.Interfaces;
 public interface IOrderRepository
 {
     public   Task<List<Order>> GetAllOrders();
-    public  Task<Order> GetOrderDetails(int id);
+    public  Task<Order> GetOrderDetails(int Id);
     public Task<bool> PlaceOrder(Order order);
 
     public Task<bool> CancelOrder(int id);
