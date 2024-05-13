ec2fc8574117b02829aa2fa1b526dc2a84ca7b58
Ravi Tambade <ravi.tambade@transflower.in>
2024-05-13 13:40:32 +0530

    dapper stored procedure

--- ConsoleSolution/ECommerce/TestDapperApp/Program.cs
+++ ConsoleSolution/ECommerce/TestDapperApp/Program.cs
@@ -2,10 +2,15 @@
 using TestDapperApp;
 using TestDapperApp.Entities;
 
-Student student = new Student { Name = "Sagar", AssignedOn = new DateTime(2024, 3, 1) };
+/*Student student = new Student { Name = "Sagar", AssignedOn = new DateTime(2024, 3, 1) };
 bool status = StudentRepository.InsertParamExecute(student); 
 //bool status=StudentRepository.Insert(student);
 if (status)
 {
     Console.WriteLine("inserted successfully");
 }
+
+*/
+
+Student theStudent=StudentRepository.GetById(1);
+Console.WriteLine(theStudent.Name + theStudent.AssignedOn);
--- ConsoleSolution/ECommerce/TestDapperApp/StudentRepository.cs
+++ ConsoleSolution/ECommerce/TestDapperApp/StudentRepository.cs
@@ -8,6 +8,38 @@ namespace TestDapperApp
 {
     public  class StudentRepository
     {
+
+
+        //Using Stored Procedure
+        public static Student GetById(int id)
+        {
+            var student=new Student();
+
+            string connectionString = "server=localhost;port=3306;user=root;password=password;database=ecommerce";
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                //Set up DynamicParameters object to pass parameters  
+                 DynamicParameters parameters = new DynamicParameters();
+                 parameters.Add("studentId", 1);
+
+                 //Execute stored procedure and map the returned result to a Customer object  
+                 student = connection.QuerySingleOrDefault<Student>("GetStudentById",
+                                                                     parameters, 
+                                                                    commandType: CommandType.StoredProcedure);
+
+                 
+
+                /* For SQL Server
+                  
+                var sql = "EXEC GetStudentById @studentId";
+                var values = new { studentId = 1};
+                var results = connection.Query(sql, values).ToList();
+                results.ForEach(s => Console.WriteLine($"{s.Id} {s.Name }"));
+
+                */
+            }
+            return student;
+        }
         public static bool InsertParamExecute(Student student)
         {
             //DML Operations
