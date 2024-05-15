1a0e2a79ff8816a0cb403e2e9510eddd641d7297
Nayankothawade <nayankothawade133@gmail.com>
2024-05-15 23:58:37 +0530

    ConsoleApp1 Delete, Update

---
+++ ConsoleSolution/ECommerce/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using TestDapperApp;
+using TestDapperApp.Entities;
+
+IEnumerable<Student> students = StudentRepository.GetStudents();
+ 
+foreach (Student student in students)
+{
+    Console.WriteLine(student.Name + " " + student.AssignedOn);
+
+}
+Student student1 = new Student();
+student1.Name = "Nayan";
+student1.AssignedOn = new DateTime(2024, 5, 14);
+StudentRepository.Insert(student1);
