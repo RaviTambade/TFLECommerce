77192882eadd0d3c681559a3a5e023968d920a0e
Ajinkya <ajinkyatambade143@gmail.com>
2024-07-28 15:43:01 +0530

    querydb added for writing general queries

---
+++ DBSolution/ECommerce/querydb.sql
@@ -0,0 +1,20 @@
+-- Select all records from the categories table
+SELECT * FROM categories;
+
+-- Select all records from the users table
+SELECT * FROM users;
+
+-- Select all records from the products table
+SELECT * FROM products;
+
+-- Select all records from the orders table
+SELECT * FROM orders;
+
+-- Select all records from the order_items table
+SELECT * FROM order_items;
+
+-- Select all records from the reviews table
+SELECT * FROM reviews;
+
+-- Select all records from the discount_codes table
+SELECT * FROM discount_codes;
