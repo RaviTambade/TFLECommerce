dc5368b7ec4d4fbe50413287dd55ec2edd602940
Ajinkya <ajinkyatambade143@gmail.com>
2024-08-01 16:30:39 +0530

    changes

--- DBSolution/ECommerce/spdb.sql
+++ DBSolution/ECommerce/spdb.sql
@@ -451,6 +451,8 @@ values(1,100);
 
 -- 12 Trigger for UPDATE on Products
 
+DROP TRIGGER before_product_update;
+
 DELIMITER //
 
 CREATE TRIGGER before_product_update
@@ -458,13 +460,14 @@ BEFORE UPDATE ON inventory
 FOR EACH ROW
 BEGIN
     INSERT INTO product_audit (product_id, action_type, old_stock_quantity, new_stock_quantity)
-    VALUES (OLD.product_id, 'UPDATE', OLD.stock_quantity + NEW.stock_quantity, NEW.stock_quantity);
+    VALUES (OLD.product_id, 'UPDATE', OLD.stock_quantity, NEW.stock_quantity);
 END//
 
 DELIMITER ;
 
+
 update inventory
-SET stock_quantity = 50
+SET stock_quantity = 600
 WHERE product_id=1;
 
 
@@ -476,10 +479,22 @@ drop table product_audit;
 
 drop table inventory;
 
-drop trigger before_product_update;
-
 drop trigger after_product_insert;
 
 
+-- 12 Trigger for DELETE on Products
+
+DELIMITER //
+CREATE TRIGGER after_product_delete
+AFTER DELETE ON inventory
+FOR EACH ROW
+BEGIN
+INSERT INTO product_audit(product_id,action_type,old_stock_quantity)
+VALUES(OLD.product_id,'DELETE', OLD.stock_quantity);
+END //
+
+DELIMITER ;
+
+DELETE FROM inventory WHERE product_id=1;
 
 
