57fb1908174fc348945946fbb8c9a62890d786b7
Ravi Tambade <ravi.tambade@transflower.in>
2024-08-01 11:53:33 +0530

    ECommerce Angular 18 updates

---
+++ CoreSolution/ProductCatalog/Repositories/IProductRepository.cs
@@ -0,0 +1,23 @@
+using Transflower.ECommerce.ProductCatalog.Entities;
+namespace Transflower.ECommerce.ProductCatalog.Repositories.Interfaces;
+
+public interface IProductRepository{
+    
+    
+    //Blocking Call
+
+    /* public List<Product> GetAll();
+     public Product GetById(int id);
+     public bool Insert(Product product);
+     public bool Update(Product product);
+     public bool Delete(int id);*/
+
+
+    //Non-Blocking call  : Aysynchronous call
+    public   Task<List<Product>> GetAll();
+    public   Task<Product> GetById(int id);
+    public   Task<bool> Insert(Product product);
+    public   Task<bool> Update(Product product);
+    public   Task<bool> Delete(int id);
+}
+
