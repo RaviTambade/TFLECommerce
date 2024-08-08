cbd04f0d41cc2beca5658501459b81b0c19f8f68
Ajinkya <ajinkyatambade143@gmail.com>
2024-08-08 14:08:30 +0530

    Sample Database Added

---
+++ DBSolution/MsSQL/Sampledb.sql
@@ -0,0 +1,4 @@
+INSERT INTO categories (name, description) VALUES
+('Electronics', 'Devices and gadgets including phones, computers, and accessories.'),
+('Books', 'A variety of books from different genres and authors.'),
+('Clothing', 'Apparel including shirts, pants, and accessories.');
