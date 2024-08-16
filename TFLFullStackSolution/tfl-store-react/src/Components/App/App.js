675c53380924f6af83869ecf6c2fd13b67fb1be4
Ravi Tambade <ravi.tambade@transflower.in>
2024-08-16 16:28:15 +0530

    test

---
+++ TFLFullStackSolution/tfl-store-react/src/Components/App/App.js
@@ -0,0 +1,26 @@
+
+import Login from '../../Components/Login/Login'
+import './App.css';
+
+function App() {
+  return (
+            <div>
+                <div class="container mt-5">
+                    <div class="row">
+                        <div class="col-sm-4">
+                        <Login></Login>
+                        </div>
+                        <div class="col-sm-8">
+                        <h2>TFL Assessment Protal</h2>      
+                <ol>
+                  <li>Online Practical Test</li>
+                  <li>Multiple Choice Questions</li>
+                  <li>Group Discussion</li>
+                </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+  );
+}
+export default App;
