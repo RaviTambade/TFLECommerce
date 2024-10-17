ec7c82f603200a72ec10eec3a09af5aa1fe0a09b
Rutuja <rutujatambade278@gmail.com>
2024-10-17 12:17:00 +0530

    Changes

--- DBSolution/MySql/Triggers.sql
+++ DBSolution/MySql/Triggers.sql
@@ -214,18 +214,30 @@ DELIMITER ;
 
 -- 10. Trigger to Automatically Update User Points Based on Order Total
 DELIMITER //
-
 CREATE TRIGGER after_order_insert
 AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
-    -- Assuming 1 point per 10 units of order total
-    UPDATE users
-    SET points = points + FLOOR(NEW.total_amount / 10)
-    WHERE id = NEW.customer_id;
-END//
+DECLARE calculate_points int default 0;
 
+    SET calculate_points = FLOOR(NEW.total_amount / 10);
+    -- Assuming 1 point per 10 units of order total
+    IF EXISTS(Select 1 from loyalty_redemptions where user_id= NEW.customer_id)
+    THEN
+    UPDATE loyalty_redemptions
+    SET points_redeemed = points_redeemed + calculate_points
+    WHERE user_id = NEW.customer_id;
+    ELSE
+   INSERT INTO loyalty_redemptions (user_id, points_redeemed, redemption_date, status) VALUES
+         (NEW.customer_id, calculate_points, NOW(), 'Pending');
+       END IF ;
+       END//
 DELIMITER ;
+
+INSERT INTO orders(customer_id, order_date, shipping_address, total_amount, shipping_date, status) VALUES
+(2, '2024-07-23', 'Manchar, pune', 1019.98, '2024-07-30', 'Shipped');
+
+DROP  trigger after_order_insert;
 
 -- 11. Trigger for Insert on Products 
 -- DROP TRIGGER after_product_insert;
