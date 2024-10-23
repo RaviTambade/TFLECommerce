ebbf96b591e74e2cdad25a23edaca1c75d6db4c9
shri <patilshridhar8056@gmail.com>
2024-10-23 15:29:46 +0530

    stored procedures

--- DBSolution/MsSQL/spdb.sql
+++ DBSolution/MsSQL/spdb.sql
@@ -9,8 +9,10 @@ AS
 BEGIN
     INSERT INTO users (username, password, email, address)
     VALUES (@p_username, @p_password, @p_email, @p_address);
-END
+END;
 
+-- select * from users;
+-- exec RegisterUser @p_username = 'Ritesh_Chopra',@p_password='rc@123',@p_email='ritechC@gmail.com',@p_address='Jalandar'
 
  -- 2. Creating a Stored Procedure for User Login
 
@@ -24,29 +26,26 @@ BEGIN
     WHERE username = @p_username AND password = @p_password;
 END;
 
-
+-- exec LoginUser @p_username='asdfsf', @p_password='rc@123';
 
 
 -- 3. Creating a Stored Procedure for Updating User Information
 
-
 CREATE PROCEDURE UpdateUserInfo
     @p_user_id INT,
-    @p_email VARCHAR(100),
+    @p_password VARCHAR(15),
+	@p_email VARCHAR(100),
     @p_address VARCHAR(255)
 AS
 BEGIN
     UPDATE users
-    SET email = @p_email, address = @p_address
+    SET password=@p_password, email= @p_email, address = @p_address
     WHERE id = @p_user_id;
 END;
-
+exec UpdateUserInfo @p_user_id='23',@p_password='Pass@123',@p_email='RiteshChopra@gmail.com',@p_address='Pune';
 
 
 -- 4. Creating a Stored Procedure for Retrieving Order Details
-
-DROP PROCEDURE IF EXISTS GetOrderDetails;
-
 
 CREATE PROCEDURE GetOrderDetails
     @p_order_id INT
@@ -58,9 +57,9 @@ BEGIN
     JOIN order_items oi ON o.id = oi.order_id
     JOIN products p ON oi.item_id = p.id
     WHERE o.id = @p_order_id;
-END 
+END; 
 
-EXEC GetOrderDetails @p_order_id = 2
+EXEC GetOrderDetails @p_order_id = 5;
 
 
 
@@ -75,7 +74,7 @@ BEGIN
     WHERE stock < @p_threshold;
 END;
 
-EXEC LowStockAlert @p_threshold = 10;
+EXEC LowStockAlert @p_threshold = 100;
 
 
 
@@ -97,8 +96,8 @@ BEGIN
     VALUES (@p_product_id, @p_user_id, @p_rating, @p_review);
 END;
 
-
-EXEC AddProductReview @p_product_id = 1, @p_user_id = 2, @p_rating = 5, @p_review = 'Great product!';
+-- select * from reviews;
+-- EXEC AddProductReview @p_product_id = 4, @p_user_id = 3, @p_rating = 4, @p_review = '5 strar product';
 
 
 -- 7. Creating a Stored Procedure for Monthly Sales Aggregation
@@ -120,6 +119,47 @@ BEGIN
 END;
 
 
-EXEC MonthlySalesReport @p_year = 2024, @p_month = 8;
+-- EXEC MonthlySalesReport @p_year = 2024, @p_month = 8;
 
-select * from products;
+-- select * from products;
+
+-- 8. Creating a Stored Procedure to apply discount
+
+CREATE PROCEDURE ApplyDiscount
+    @p_order_id INT,
+    @p_discount_code VARCHAR(50)
+AS
+BEGIN
+    DECLARE @v_discount DECIMAL(5, 2);
+    DECLARE @v_total DECIMAL(10, 2);
+    -- Retrieve discount percentage from the discount_codes table
+    SELECT @v_discount = discount_percentage
+    FROM discount_codes
+    WHERE code = @p_discount_code 
+    AND GETDATE() BETWEEN start_date AND end_date;
+    IF @v_discount IS NOT NULL
+    BEGIN
+        -- Get the current total amount of the order
+        SELECT @v_total = total_amount
+        FROM orders
+        WHERE id = @p_order_id;
+        -- Apply the discount
+        SET @v_total = @v_total - (@v_total * (@v_discount / 100));
+        -- Update the order with the discounted amount
+        UPDATE orders
+        SET total_amount = @v_total
+        WHERE id = @p_order_id;
+    END
+    ELSE
+    BEGIN
+        -- Throw an error for invalid or expired discount code
+        RAISERROR ('Invalid or expired discount code.', 16, 1);
+    END
+END
+
+exec ApplyDiscount
+    @p_order_id =1,
+    @p_discount_code='DIWALI21';
+
+select * from orders;
+select * from discount_codes;
