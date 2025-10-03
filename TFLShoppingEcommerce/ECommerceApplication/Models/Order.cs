bdb415765c4356a15fd66ae80c690bb85123db20
sanika-bhor <bhorsanika0239@gmail.com>
2025-10-03 10:57:11 +0530

    order processing

--- TFLShoppingEcommerce/ECommerceApplication/Models/Order.cs
+++ TFLShoppingEcommerce/ECommerceApplication/Models/Order.cs
@@ -7,7 +7,7 @@ namespace ECommerceApplication.Models
         public DateOnly orderDate { get; set; }
         public string ShippingAddress { get; set; }
         public double TotalAmount { get; set; }
-        public DateOnly ShippingDate { get; set; }
+        public DateOnly? ShippingDate { get; set; }
         public string status { get; set; }
         public string Address{ get; set; }
     }
